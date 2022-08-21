ce5f99820a331ddf817c74a95a91828af683e884
Rajurk <rajurk044@gmail.com>
2022-08-21 22:29:40 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import {useContext} from 'react'
 import './App.css';
 import Intro from './components/intro/Intro'
 import About from './components/about/About'
 import ProductList from './components/productList/ProductList'
+import Contact from './components/contact/Contact'
+import Toggle from './components/toggle/Toggle'
+import { ThemeContext } from './Context'
 
 function App() {
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
   return (
-    <>
+    <div style={{
+      backgroundColor: darkMode ? "#222" : "#fff", color: darkMode && "#fff"}}>
+      <Toggle/>
       <Intro/>
       <About/>
       <ProductList/>
-    </> 
+      <Contact/>
+    </div> 
   );
 }
 
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,74 @@
+import React, { useRef, useState, useContext} from 'react'
+import Address from '../../img/address.png'
+import Email from '../../img/email.png'
+import Phone from '../../img/phone.png'
+import './contact.css'
+import emailjs from "emailjs-com";
+import { ThemeContext } from '../../Context'
+
+function Contact() {
+  
+  const formRef = useRef(); 
+  const [done, setDone] = useState(false)
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_50ihfsn",
+        "template_71azzrg",
+        formRef.current,
+        "G-bvzskLEKIAIadXp"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+
+  return (
+    <div className="c">
+       <div className="c-bg"></div>
+       <div className="c-wrapper">
+           <div className="c-left">
+             <h1 className="c-title">Let's discuss your project</h1>
+             <div className="c-info">
+                <div className="c-info-item">
+                    <img src={Phone} alt="" className="c-icon"/> +91 978 835 1283
+                </div>
+                <div className="c-info-item">
+                    <img src={Email} alt="" className="c-icon"/> rajurk044@gmail.com
+                </div>
+                <div className="c-info-item">
+                    <img src={Address} alt="" className="c-icon"/> 24A Murugan nagar 2nd Street, Rathinapuri, Coimbatore, 641027
+                </div>
+             </div>
+           </div>
+           <div className="c-right">
+            <p className="c-desc">
+                <b>What’s your story?</b> Get in touch. Always available for
+                    freelancing if the right project comes along. me.
+            </p>
+            <form ref={formRef} onSubmit={handleSubmit}>
+                <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name"/>
+                <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email"/>
+                <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject"/>
+                <textarea style={{backgroundColor: darkMode && "#333"}} row="5" placeholder="Message" name="user_message"/>
+                <button>Submit</button>
+                <div>{done && "Thank you..."}</div>                
+            </form>
+           </div>
+       </div>
+    </div>
+  )
+}
+
+export default Contact
